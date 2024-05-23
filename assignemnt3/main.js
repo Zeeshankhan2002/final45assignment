98899863fd1b9bf1c03c6f51de77d9f7027d65de
Zeeshankhan2002 <165414353+Zeeshankhan2002@users.noreply.github.com>
2024-05-23 17:34:20 +0500

    first commit

---
+++ assignemnt3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the femous quote and its author
+let quote = "nothing is impossible because impossible itself I AM POSSIBLE";
+let author = "Zeeshan khan";
+// printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignemnt3/main.ts
@@ -0,0 +1,9 @@
+// storing the femous quote and its author
+
+let quote:string = "nothing is impossible because impossible itself I AM POSSIBLE"
+
+let author:string = "Zeeshan khan"
+
+// printing the message
+
+console.log(`"${quote}" - ${author}`);
